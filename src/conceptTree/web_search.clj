6cd10a7b2b9e66ab039dd44023dc497e153242df
Erick Santana <neves.erick@gmail.com>
2012-05-14 15:47:00 -0300

    Update src/conceptTree/web_search.clj

--- src/conceptTree/web_search.clj
+++ src/conceptTree/web_search.clj
@@ -1,25 +1,25 @@
-;;abreviations:
-;;c=con
-;;m-con=main-con
-;;con-t=concept-tree
-;;ret=retrieve
-;;mcraw=metacrawler
 (ns conceptTree.web-search
-  (:use [clojure.contrib.str-utils :only (str-join)]))
+	(:use [clojure.contrib.str-utils :only (str-join)]))
 
-(defn fetch-url [address]
+(defn fetch-url
+	"returns the content of a web page as a string"
+	[address]
 	(with-open [stream (.openStream (java.net.URL. address))]
 		(let  [buf (java.io.BufferedReader.
 			(java.io.InputStreamReader. stream))]
 			(apply str (line-seq buf)))))
 
-(defn ret-links-from-mcraw [m-con]
+(defn retrieve-links
+	"make a search using the metacrawler search engine and returns a list with the links from the search's result"
+	[m-con]
 	(let [compl "is+a"]
 		(re-seq #"(?<=<a class=\"resultTitle\" data-icl-coi=\"540\" data-icl-cop=\"results-main\" href=\")(.*?)(?= target=\"_blank\"\>)" 
-  		(fetch-url (str "http://www.metacrawler.com/info.metac.test.c11/search/web?fcoid=485&fpid=1&qexact=" m-con compl "&qlang=en")))))
+		(fetch-url (str "http://www.metacrawler.com/info.metac.test.c11/search/web?fcoid=485&fpid=1&qexact=" m-con compl "&qlang=en")))))
 
-(defn retrieve-concept-tree-metacrawler [m-con]
-  (for [link (ret-links-from-mcraw m-con)]
+(defn build-concept-tree
+	"reads the content of all the pages from the links"
+	[m-con]
+	(for [link (retrieve-links m-con)]
 		(let [inner-link (nth (.split (nth link 0) "ru=") 1) unallowed-symbols ",|\\.|:|;|_|+|=|&|#|$|%|!|@"]
 			(re-seq
 				(java.util.regex.Pattern/compile (str "(?<=" m-con " is a " ")(.*?)(?=[" unallowed-symbols "])") java.util.regex.Pattern/CASE_INSENSITIVE)
@@ -27,12 +27,11 @@
 					(java.net.URLDecoder/decode
 						(nth (.split inner-link "&amp") 0)))))))
 
-(defn create-con-t-str [m-con]
-  (map #(str "=" (nth (nth %1 0) 0) "=") (ret-con-t-from-mcraw-links-for-con m-con)))
+(defn create-concept-tree-str [m-con]
+	(map #(str "=" (nth (nth %1 0) 0) "=") (build-concept-tree m-con)))
 
-;;(use '[clojure.contrib.str-utils :only (str-join)])
-(defn wrap-con-t [m-con]
-  (str "[=" m-con "=[" (str-join \, (create-con-t-str m-con)) "]]"))
+(defn wrap-concept-tree [m-con]
+	(str "[=" m-con "=[" (str-join \, (create-concept-tree-str m-con)) "]]"))
 
-(defn make-con-t-for-con [m-con]
-  (wrap-con-t m-con))
+(defn make-concept-tree [m-con]
+	(wrap-concept-tree m-con))
